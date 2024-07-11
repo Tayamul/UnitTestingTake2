2619be8a7d9ae43a984dfc482718c7e06f49df85
Tayamul <rai_tayamul@hotmail.com>
2024-07-11 12:43:03 +0100

    declare private variables

--- src/main/scala/CGTCalculator.scala
+++ src/main/scala/CGTCalculator.scala
@@ -1,3 +1,6 @@
 class CGTCalculator {
+  private val basicRate: Double = 0.1
+  private val higherRate: Double = 0.2
 
+  private val annualIncomeThreshold: Double = 50270
 }
--- src/test/scala/CGTCalculatorSpec.scala
+++ src/test/scala/CGTCalculatorSpec.scala
@@ -1,3 +1,8 @@
 class CGTCalculatorSpec {
+  private val basicRate: Double = 0.1
+  private val higherRate: Double = 0.2
+
+  private val annualIncomeThreshold: Double = 50270
+
   val cgtCalculator: CGTCalculator = new CGTCalculator
 }
