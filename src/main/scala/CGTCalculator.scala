400bba4169acbebf14b36b8921a98a9680296e93
Tayamul <rai_tayamul@hotmail.com>
2024-07-11 18:32:30 +0100

    implement happy path tests for calculating capital gains tax

--- src/main/scala/CGTCalculator.scala
+++ src/main/scala/CGTCalculator.scala
@@ -1,6 +1,33 @@
-class CGTCalculator {
-  private val basicRate: Double = 0.1
-  private val higherRate: Double = 0.2
+//class CGTCalculator {
+//  private val cgtLowerRate: Double = 0.1
+//  private val cgtHigherRate: Double = 0.2
+//
+//  private val cgtAllowance: Double = 3000
+//  private val capitalGainsWithinThreshold: Double = 2000
+//  private val capitalGains: Double = 20000
+//  private val annualIncomeThreshold: Double = 50270
+//
+//  def calculateCapitalGains (income: Double): Double = {
+//    if (income <= 0) 404
+//    else if (income <= annualIncomeThreshold && income >= 3000) (capitalGains - cgtAllowance) * cgtLowerRate
+//    else if (income >= annualIncomeThreshold) (capitalGains - cgtAllowance) * cgtHigherRate
+//    else if (capitalGainsWithinThreshold <= cgtAllowance) 0
+//    else 404
+//  }
+//}
 
+class CGTCalculator {
+
+  private val cgtLowerRate: Double = 0.1
+  private val cgtHigherRate: Double = 0.2
+
+  private val cgtAllowance: Double = 3000
   private val annualIncomeThreshold: Double = 50270
+
+  def calculateCGT (income: Double, capitalGains: Double): Double = {
+    if (capitalGains <= 3000 && capitalGains > 0) 0
+    else if (income <= annualIncomeThreshold) (capitalGains - cgtAllowance) * cgtLowerRate
+    else if (income >= annualIncomeThreshold) (capitalGains - cgtAllowance) * cgtHigherRate
+    else 404
+  }
 }
--- src/test/scala/CGTCalculatorSpec.scala
+++ src/test/scala/CGTCalculatorSpec.scala
@@ -1,8 +1,88 @@
-class CGTCalculatorSpec {
-  private val basicRate: Double = 0.1
-  private val higherRate: Double = 0.2
+import org.scalatest.wordspec.AnyWordSpec
 
+//class CGTCalculatorSpec extends AnyWordSpec {
+//  private val cgtLowerRate: Double = 0.1
+//  private val cgtHigherRate: Double = 0.2
+//
+//  private val cgtAllowance: Double = 3000
+//  private val capitalGains: Double = 20000
+//  private val annualIncomeThreshold: Double = 50270
+//
+//  val cgtCalculator: CGTCalculator = new CGTCalculator
+//
+//  "CGTCalculator.calculateCapitalGains" should {
+//    "return the total amount of capital gains tax to pay" when {
+//      s"the overall annual income is below £${annualIncomeThreshold.toInt}" in {
+//        val income: Double = 30000
+//        val result: Double = cgtCalculator.calculateCapitalGains(income)
+//        assert(result == (capitalGains - cgtAllowance) * cgtLowerRate)
+//      }
+//    }
+//    "return the total amount of capital gains tax to pay" when {
+//      s"the overall annual income is above £${annualIncomeThreshold.toInt}" in {
+//        val income: Double = 60000
+//        val result: Double = cgtCalculator.calculateCapitalGains(income)
+//        assert(result == (capitalGains - cgtAllowance) * cgtHigherRate)
+//      }
+//    }
+//    "return the total amount of capital gains tax to pay" when {
+//      s"the capital gains from shares is within the Capital Gains Tax Allowance rate of £${cgtAllowance.toInt}" in {
+//        val income: Double = 2000
+//        val result: Double = cgtCalculator.calculateCapitalGains(income)
+//        assert(result == 0)
+//      }
+//    }
+//    "return a 404 error message" when {
+//      "the income is 0" in {
+//        val income: Double = 0
+//        val result: Double = cgtCalculator.calculateCapitalGains(income)
+//        assert(result == 404)
+//      }
+//    }
+//    "return a 404 error message" when {
+//      "the income is non-negative integer" in {
+//        val income: Double = -30000
+//        val result: Double = cgtCalculator.calculateCapitalGains(income)
+//        assert(result == 404)
+//      }
+//    }
+//
+//  }
+//}
+
+class CGTCalculatorSpec extends AnyWordSpec {
+  private val cgtLowerRate: Double = 0.1
+  private val cgtHigherRate: Double = 0.2
+
+  private val cgtAllowance: Double = 3000
   private val annualIncomeThreshold: Double = 50270
 
   val cgtCalculator: CGTCalculator = new CGTCalculator
+
+  "CGTCalculator.calculateCGT" should {
+    "return the total amount of capital gains tax to pay" when {
+      "the overall annual income is below £50,270 AND the capital gains is £20,000" in {
+        val income: Double = 30000
+        val capitalGains: Double = 20000
+        val result = cgtCalculator.calculateCGT(income, capitalGains)
+        assert(result == (capitalGains - cgtAllowance) * cgtLowerRate)
+      }
+    }
+    "return the total amount of capital gains tax to pay" when {
+      "the overall annual income is above £50,270 AND the capital gains is £20,000" in {
+        val income: Double = 75000
+        val capitalGains: Double = 20000
+        val result = cgtCalculator.calculateCGT(income, capitalGains)
+        assert(result == (capitalGains - cgtAllowance) * cgtHigherRate)
+      }
+    }
+    "return the total amount of capital gains tax to pay" when {
+      s"the capital gains from shares is within the Capital Gains Tax Allowance rate of £${cgtAllowance.toInt}" in {
+        val income: Double = 75000
+        val capitalGains: Double = 2000
+        val result = cgtCalculator.calculateCGT(income, capitalGains)
+        assert(result == 0)
+      }
+    }
+  }
 }
