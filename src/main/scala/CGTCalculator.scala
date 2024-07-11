fb0c3490ffc853f64e876a4ca24253fd7c43086b
Tayamul <rai_tayamul@hotmail.com>
2024-07-11 18:34:05 +0100

    remove the older code

--- src/main/scala/CGTCalculator.scala
+++ src/main/scala/CGTCalculator.scala
@@ -1,21 +1,3 @@
-//class CGTCalculator {
-//  private val cgtLowerRate: Double = 0.1
-//  private val cgtHigherRate: Double = 0.2
-//
-//  private val cgtAllowance: Double = 3000
-//  private val capitalGainsWithinThreshold: Double = 2000
-//  private val capitalGains: Double = 20000
-//  private val annualIncomeThreshold: Double = 50270
-//
-//  def calculateCapitalGains (income: Double): Double = {
-//    if (income <= 0) 404
-//    else if (income <= annualIncomeThreshold && income >= 3000) (capitalGains - cgtAllowance) * cgtLowerRate
-//    else if (income >= annualIncomeThreshold) (capitalGains - cgtAllowance) * cgtHigherRate
-//    else if (capitalGainsWithinThreshold <= cgtAllowance) 0
-//    else 404
-//  }
-//}
-
 class CGTCalculator {
 
   private val cgtLowerRate: Double = 0.1
