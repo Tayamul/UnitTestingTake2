7ba09611d4cc0454a7d85d7dfb9ffdb810fc014f
Tayamul <100432502+Tayamul@users.noreply.github.com>
2024-07-11 11:08:44 +0100

    TaxCalculator MVP (#1)
    * change .gitignore
    
    * complete the TaxCalculatorSpec test and logic
    
    * complete the test and logic for isHigherRateTaxpayer method
    
    * complete the formattedCurrentTaxAllowance test and logic

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -13,18 +13,27 @@ class TaxCalculator {
 
   // A method to calculate the total amount of tax to be paid, returned as a double
   def calculateTax(income: Double): Double = {
-    ???
+    if (income <= 0) 404
+    else if (income <= personalAllowance) income * personalAllowanceRate
+    else if (income <= basicRateLimit) (income - personalAllowance) * basicRate
+    else if (income <= higherRateLimit) (((basicRateLimit - personalAllowance) * basicRate ) + ((income - basicRateLimit) * higherRate))
+    else (((basicRateLimit - personalAllowance) * basicRate) + ((higherRateLimit - basicRateLimit) * higherRate) + ((income - higherRateLimit) * additionalRate))
   }
 
   // A method which can tell you if someone is a higher rate taxpayer
   def isHigherRateTaxpayer(income: Double): Boolean = {
-    ???
+    income >= basicRateLimit && income <= higherRateLimit
   }
 
   // A method that will return a string with the income limit of their current tax band.
   // The return will also be formatted, E.g: "£12,500" or "No limit"
   def formattedCurrentTaxAllowance(income: Double): String = {
-    ???
+    if (income == 0) "Must be an error! No one works for free."
+    else if (income < 0 ) "Invalid input: Salary must be a non-negative number."
+    else if (income <= personalAllowance) s"Income limit for your current tax band: £0 - £$personalAllowance."
+    else if (income <= basicRateLimit) s"Income limit for your current tax band: £${personalAllowance + 1} - £$basicRateLimit."
+    else if (income <= higherRateLimit) s"Income limit for your current tax band: £${basicRateLimit + 1} - £$higherRateLimit."
+    else if (income >= higherRateLimit) s"Income limit for your current tax band: Above £$higherRateLimit."
+    else "Something went wrong! Please try again."
   }
-
 }
